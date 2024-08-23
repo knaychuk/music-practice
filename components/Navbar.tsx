5791b4daff93e2364e6ccd845d63bc2e3fc4274b
Kate <knaychuk@gmail.com>
2024-08-23 01:04:09 -0700

    setup navbar links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,26 @@
+'use client'
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 const Navbar = () => {
+  const pathname = usePathname();
+
+  const navItems = [
+    { name: 'Home', path: '/home' },
+    { name: 'Practice', path: '/practice' },
+    { name: 'Sheet Music', path: '/sheet-music' },
+    { name: 'Tuner', path: '/tuner' },
+  ];
+
   return (
-    <div>Navbar</div>
+    <div>
+      {navItems.map(item => (
+        <Link key={item.path} href={item.path} className={`text-black ${pathname === item.path ? 'text-red-500' : 'text-black'}`}>
+          {item.name}
+        </Link>
+      ))}
+    </div>
   )
 }
 export default Navbar
