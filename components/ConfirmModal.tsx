9d25c6bff82da3e060535a2b30703a70011d8db9
Kate <knaychuk@gmail.com>
2024-09-23 01:27:13 -0700

    convert confirm modal to component

--- components/ConfirmModal.tsx
+++ components/ConfirmModal.tsx
@@ -1,6 +1,30 @@
-const ConfirmModal = () => {
+export interface ConfirmModalProps {
+  confirmFunction: () => void;
+  cancelFunction: () => void;
+}
+
+const ConfirmModal = ({ confirmFunction, cancelFunction }:ConfirmModalProps) => {
   return (
-    <div>ConfirmModal</div>
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+        <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full">
+          <h3 className="text-lg font-bold text-gray-900 mb-4">Confirm Deletion</h3>
+          <p className="text-gray-700 mb-6">Are you sure you want to delete this entry?</p>
+          <div className="flex justify-end space-x-4">
+            <button
+              onClick={confirmFunction}
+              className="bg-red-600 text-white px-4 py-2 rounded-md hover:bg-red-700 transition-colors"
+            >
+              Confirm
+            </button>
+            <button
+              onClick={cancelFunction}
+              className="bg-gray-300 text-gray-700 px-4 py-2 rounded-md hover:bg-gray-400 transition-colors"
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
   )
 }
 export default ConfirmModal
--- components/PracticeEntries/PracticeEntryCard.tsx
+++ components/PracticeEntries/PracticeEntryCard.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { TbEdit } from "react-icons/tb";
 import { GoDash } from "react-icons/go";
 import { FaRegTrashAlt } from "react-icons/fa";
+import ConfirmModal from "../ConfirmModal";
 
 export interface PracticeEntryCardProps {
   entry: {
@@ -41,7 +42,7 @@ export const calculatePracticeTime = (startTime: string, endTime: string): {hour
   return { hours, minutes };
 }
 
-const PracticeEntryCard = ({entry, handleUpdate}:PracticeEntryCardProps) => {
+const PracticeEntryCard = ({ entry, handleUpdate }:PracticeEntryCardProps) => {
   const [isEditing, setIsEditing] = useState(false);
   const [day, setDay] = useState('');
   const [date, setDate] = useState(entry.date);
@@ -91,6 +92,10 @@ const PracticeEntryCard = ({entry, handleUpdate}:PracticeEntryCardProps) => {
 
   const handleShowModal = () => {
     setShowConfirmModal(true);
+  }
+
+  const handleCloseModal = () => {
+    setShowConfirmModal(false);
   }
 
   const handleDelete = async () => {
@@ -188,26 +193,7 @@ const PracticeEntryCard = ({entry, handleUpdate}:PracticeEntryCardProps) => {
        </div>
       }
       {showConfirmModal && 
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-        <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full">
-          <h3 className="text-lg font-bold text-gray-900 mb-4">Confirm Deletion</h3>
-          <p className="text-gray-700 mb-6">Are you sure you want to delete this entry?</p>
-          <div className="flex justify-end space-x-4">
-            <button
-              onClick={handleDelete}
-              className="bg-red-600 text-white px-4 py-2 rounded-md hover:bg-red-700 transition-colors"
-            >
-              Confirm
-            </button>
-            <button
-              onClick={() => setShowConfirmModal(false)}
-              className="bg-gray-300 text-gray-700 px-4 py-2 rounded-md hover:bg-gray-400 transition-colors"
-            >
-              Cancel
-            </button>
-          </div>
-        </div>
-      </div>
+        <ConfirmModal confirmFunction={handleDelete} cancelFunction={handleCloseModal} />
       }
     </div>
     
